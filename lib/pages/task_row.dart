1d46c99ff064283c8ed0c7069bc4b29586814b1a
Hinriksnaer <hinriksnaer@gmail.com>
2019-03-24 10:18:54 +0000

    implemented spagetti

--- lib/pages/task_row.dart
+++ lib/pages/task_row.dart
@@ -5,40 +5,47 @@ class TaskRow extends StatelessWidget {
   final Task task;
   final double dotSize = 12.0;
   final Animation<double> animations;
+  final Function onClick;
 
-  const TaskRow({Key key, this.task, this.animations}) : super(key: key);
+  const TaskRow({Key key, this.task, this.animations, this.onClick}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return new FadeTransition(
-      opacity: animations,
-      child: new SizeTransition(
-        sizeFactor: animations,
-        child: new Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          child: new Row(
-            children: <Widget>[
-              new Padding(
-                padding:
-                    EdgeInsets.symmetric(horizontal: 32.0 - dotSize / 2),                                           
-              ),
-              new Padding(padding: EdgeInsets.only(left: 0.0),),
-              new Expanded(
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[                    
-                    new Text(
-                      task.name, 
-                      style: new TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600),
-                    ),
-                    new Text(
-                      task.category,
-                      style: new TextStyle(fontSize: 12.0, color: Colors.grey),                      
-                    )
-                  ],
+    return new GestureDetector(
+      onTap: onClick,
+      child: new FadeTransition(
+        opacity: animations,
+        child: new SizeTransition(
+          sizeFactor: animations,
+          child: new Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: new Row(
+              children: <Widget>[
+                new Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 32.0 - dotSize / 2),
                 ),
-              ),
-            ],
+                new Padding(
+                  padding: EdgeInsets.only(left: 0.0),
+                ),
+                new Expanded(
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      new Text(
+                        task.name,
+                        style: new TextStyle(
+                            fontSize: 24.0, fontWeight: FontWeight.w600),
+                      ),
+                      new Text(
+                        task.category,
+                        style:
+                            new TextStyle(fontSize: 12.0, color: Colors.grey),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
